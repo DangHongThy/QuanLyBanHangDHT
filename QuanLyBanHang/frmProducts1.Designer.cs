41d12b093253e84b3bf36ece8ec865e8046e5f6d
DangHongThy <1554050096thy@ou.edu.vn>
2019-03-22 11:11:02 +0700

    gop form

---
+++ QuanLyBanHang/frmProducts1.Designer.cs
@@ -0,0 +1,288 @@
+﻿namespace QuanLyBanHang
+{
+    partial class frmProducts1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.txtDonvitinh = new System.Windows.Forms.TextBox();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.txtMaloaisp = new System.Windows.Forms.TextBox();
+            this.txtDongia = new System.Windows.Forms.TextBox();
+            this.txtId = new System.Windows.Forms.TextBox();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.dgvProducts1 = new System.Windows.Forms.DataGridView();
+            this.CotCanLay = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.tensp = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.donvitinh = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dongia = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.maloaisp = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.CotXoa = new System.Windows.Forms.DataGridViewButtonColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvProducts1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            this.splitContainer1.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.txtDonvitinh);
+            this.splitContainer1.Panel1.Controls.Add(this.txtName);
+            this.splitContainer1.Panel1.Controls.Add(this.txtMaloaisp);
+            this.splitContainer1.Panel1.Controls.Add(this.txtDongia);
+            this.splitContainer1.Panel1.Controls.Add(this.txtId);
+            this.splitContainer1.Panel1.Controls.Add(this.btnUpdate);
+            this.splitContainer1.Panel1.Controls.Add(this.btnAdd);
+            this.splitContainer1.Panel1.Controls.Add(this.label5);
+            this.splitContainer1.Panel1.Controls.Add(this.label4);
+            this.splitContainer1.Panel1.Controls.Add(this.label3);
+            this.splitContainer1.Panel1.Controls.Add(this.label2);
+            this.splitContainer1.Panel1.Controls.Add(this.label1);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.dgvProducts1);
+            this.splitContainer1.Size = new System.Drawing.Size(779, 449);
+            this.splitContainer1.SplitterDistance = 223;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // txtDonvitinh
+            // 
+            this.txtDonvitinh.Location = new System.Drawing.Point(123, 151);
+            this.txtDonvitinh.Name = "txtDonvitinh";
+            this.txtDonvitinh.Size = new System.Drawing.Size(100, 20);
+            this.txtDonvitinh.TabIndex = 7;
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(123, 92);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(100, 20);
+            this.txtName.TabIndex = 7;
+            // 
+            // txtMaloaisp
+            // 
+            this.txtMaloaisp.Location = new System.Drawing.Point(466, 89);
+            this.txtMaloaisp.Name = "txtMaloaisp";
+            this.txtMaloaisp.Size = new System.Drawing.Size(179, 20);
+            this.txtMaloaisp.TabIndex = 7;
+            // 
+            // txtDongia
+            // 
+            this.txtDongia.Location = new System.Drawing.Point(466, 33);
+            this.txtDongia.Name = "txtDongia";
+            this.txtDongia.Size = new System.Drawing.Size(179, 20);
+            this.txtDongia.TabIndex = 7;
+            // 
+            // txtId
+            // 
+            this.txtId.Location = new System.Drawing.Point(123, 40);
+            this.txtId.Name = "txtId";
+            this.txtId.Size = new System.Drawing.Size(100, 20);
+            this.txtId.TabIndex = 7;
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Location = new System.Drawing.Point(658, 169);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(75, 23);
+            this.btnUpdate.TabIndex = 6;
+            this.btnUpdate.Text = "Sửa";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(492, 169);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(75, 23);
+            this.btnAdd.TabIndex = 5;
+            this.btnAdd.Text = "Thêm";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(41, 152);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(63, 13);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Don Vi Tinh";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(398, 92);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(62, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Ma Loai SP";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(395, 40);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(46, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Don Gia";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(41, 92);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(40, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "TenSP";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(38, 40);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(36, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "MaSP";
+            // 
+            // dgvProducts1
+            // 
+            this.dgvProducts1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvProducts1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.CotCanLay,
+            this.tensp,
+            this.donvitinh,
+            this.dongia,
+            this.maloaisp,
+            this.CotXoa});
+            this.dgvProducts1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dgvProducts1.Location = new System.Drawing.Point(0, 0);
+            this.dgvProducts1.Name = "dgvProducts1";
+            this.dgvProducts1.Size = new System.Drawing.Size(779, 222);
+            this.dgvProducts1.TabIndex = 0;
+            this.dgvProducts1.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvProducts1_CellClick);
+            this.dgvProducts1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvProducts1_CellContentClick);
+            // 
+            // CotCanLay
+            // 
+            this.CotCanLay.DataPropertyName = "MaSP";
+            this.CotCanLay.HeaderText = "Mã SP";
+            this.CotCanLay.Name = "CotCanLay";
+            this.CotCanLay.Width = 50;
+            // 
+            // tensp
+            // 
+            this.tensp.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.tensp.DataPropertyName = "TenSP";
+            this.tensp.HeaderText = "Tên SP";
+            this.tensp.Name = "tensp";
+            // 
+            // donvitinh
+            // 
+            this.donvitinh.DataPropertyName = "Donvitinh";
+            this.donvitinh.HeaderText = "Đơn vị tính";
+            this.donvitinh.Name = "donvitinh";
+            // 
+            // dongia
+            // 
+            this.dongia.DataPropertyName = "Dongia";
+            this.dongia.HeaderText = "Mã Loại SP";
+            this.dongia.Name = "dongia";
+            this.dongia.Width = 50;
+            // 
+            // maloaisp
+            // 
+            this.maloaisp.DataPropertyName = "MaLoaiSP";
+            this.maloaisp.HeaderText = "Đơn Giá";
+            this.maloaisp.Name = "maloaisp";
+            this.maloaisp.Width = 150;
+            // 
+            // CotXoa
+            // 
+            this.CotXoa.HeaderText = "Xóa";
+            this.CotXoa.Name = "CotXoa";
+            this.CotXoa.Width = 50;
+            // 
+            // frmProducts1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(779, 449);
+            this.Controls.Add(this.splitContainer1);
+            this.Name = "frmProducts1";
+            this.Text = "frmProducts1";
+            this.Load += new System.EventHandler(this.frmProducts1_Load);
+            //this.Click += new System.EventHandler(this.frmProducts1_Click);
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel1.PerformLayout();
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvProducts1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.TextBox txtDonvitinh;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.TextBox txtMaloaisp;
+        private System.Windows.Forms.TextBox txtDongia;
+        private System.Windows.Forms.TextBox txtId;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView dgvProducts1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn CotCanLay;
+        private System.Windows.Forms.DataGridViewTextBoxColumn tensp;
+        private System.Windows.Forms.DataGridViewTextBoxColumn donvitinh;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dongia;
+        private System.Windows.Forms.DataGridViewTextBoxColumn maloaisp;
+        private System.Windows.Forms.DataGridViewButtonColumn CotXoa;
+    }
+}
---
+++ QuanLyBanHang/frmProducts1.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using DAO;
+using BUS;
+using DTO;
+
+namespace QuanLyBanHang
+{
+    public partial class frmProducts1 : Form
+    {
+        Products1BUS proBUS = new Products1BUS();
+        public frmProducts1()
+        {
+            InitializeComponent();
+        }
+        
+        private void frmProducts1_Load(object sender, EventArgs e)
+        {
+            this.Show();
+            this.Enabled = false;
+
+            frmLogIn frmLog = new frmLogIn();
+            DialogResult result = frmLog.ShowDialog();
+
+            if (result == System.Windows.Forms.DialogResult.OK)
+            {
+                this.Enabled = true;
+            }
+            else
+            {
+                Application.Exit();
+            }
+            List<Products1> list = proBUS.LoadProducts1();
+            dgvProducts1.DataSource = list;
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            string masp, tensp, donvitinh, dongia, maloaisp;
+            
+            masp = txtId.Text;
+            tensp = txtName.Text;
+            donvitinh = txtDonvitinh.Text;
+            dongia = txtDongia.Text;
+            maloaisp = txtMaloaisp.Text;
+
+            Products1 s = new Products1(masp, tensp, donvitinh, dongia, maloaisp);
+            int numberOfRows = proBUS.Add(s);
+
+            //if (numberOfRows > 0)
+            //    MessageBox.Show("Them thanh cong!");
+            //else
+            //    MessageBox.Show("Them that bai!");
+            if (numberOfRows > 0)
+            {
+                List<Products1> list = proBUS.LoadProducts1();
+                dgvProducts1.DataSource = list;
+                MessageBox.Show("Them thanh cong");
+            }
+            else
+                MessageBox.Show("Them that bai");
+        }
+
+        private void dgvProducts1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            int col = e.ColumnIndex;
+
+            if (dgvProducts1.Columns[col] is DataGridViewButtonColumn && dgvProducts1.Columns[col].Name == "CotXoa")
+            {
+                int row = e.RowIndex;
+                String id = dgvProducts1.Rows[row].Cells["Cotcanlay"].Value.ToString();
+                int numberOfRows = proBUS.Delete(id);
+
+                if (numberOfRows > 0)
+                {
+                    List<Products1> list = proBUS.LoadProducts1();
+                    dgvProducts1.DataSource = list;
+                    MessageBox.Show("Da xoa");
+                }
+                else
+                    MessageBox.Show("Xoa that bai");
+            }
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            string masp, tensp, donvitinh, dongia, maloaisp;
+            
+            masp = txtId.Text;
+            tensp = txtName.Text;
+            donvitinh = txtDonvitinh.Text;
+            maloaisp = txtMaloaisp.Text;
+            dongia =txtDongia.Text;
+
+            Products1 s = new Products1(masp, tensp, donvitinh, dongia, maloaisp);
+            int numberOfRows = proBUS.Update(s);
+            if (numberOfRows > 0)
+            {
+                List<Products1> list = proBUS.LoadProducts1();
+                dgvProducts1.DataSource = list;
+                MessageBox.Show("Sửa thành công!");
+            }
+            else
+                MessageBox.Show("Sửa thất bại!");
+        }
+
+        private void dgvProducts1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+            string masp, tensp, donvitinh, dongia, maloaisp;
+            
+            masp = txtId.Text;
+            tensp = txtName.Text;
+            donvitinh = txtDonvitinh.Text;
+            dongia = txtDongia.Text;
+            maloaisp = txtMaloaisp.Text;
+
+            int row = e.RowIndex;
+            txtId.Text = dgvProducts1.Rows[row].Cells["CotCanLay"].Value.ToString();
+            txtName.Text = dgvProducts1.Rows[row].Cells["tensp"].Value.ToString();
+            txtDonvitinh.Text = dgvProducts1.Rows[row].Cells["donvitinh"].Value.ToString();
+            txtMaloaisp.Text = dgvProducts1.Rows[row].Cells["maloaisp"].Value.ToString();
+            txtDongia.Text = dgvProducts1.Rows[row].Cells["dongia"].Value.ToString();
+        }
+
+        
+    }
+}
